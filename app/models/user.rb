fff84d1e134521c17ccbbe8766820d72c0830c92
Annerose <anb128@pitt.edu>
2012-08-08 14:31:24 -0400

    Finish updating users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,12 @@ class User < ActiveRecord::Base
 	has_secure_password
 
 	before_save { |user| user.email = user.email.downcase }
-	# before_save { |user| user.remember_token = "dafd" } // take private section out
 	before_save :create_remember_token
 
 	validates :name, 	:presence => true, :length => { :maximum => 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, :presence => true, :format => { :with => VALID_EMAIL_REGEX },
 						:uniqueness => { :case_sensitive => false}
-
 	validates :password, :length => { :minimum => 6 }
 	validates :password_confirmation, :presence => true 
 
